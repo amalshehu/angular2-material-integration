0b6d352300a9e70a0d6fbf9109c558f2d0e21765
amalshehu <amalshehu@gmail.com>
2016-11-03 12:55:46 +0530

    import toast

--- src/client/app/about/about.component.ts
+++ src/client/app/about/about.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import {Md2Toast} from 'md2/toast/toast';
 /**
  * This class represents the lazy loaded AboutComponent.
  */
+
 @Component({
-  moduleId: module.id,
+  moduleId: String(module.id),
   selector: 'sd-about',
   templateUrl: 'about.component.html',
   styleUrls: ['about.component.css']
 })
-export class AboutComponent { }
+export class AboutComponent {
+  constructor(private toast: Md2Toast) { }
+    toastMe() {
+      this.toast.show('Toast message...');
+ }
+ }
--- src/client/app/about/about.module.ts
+++ src/client/app/about/about.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
+import {Md2Toast} from 'md2/toast/toast';
+import { Md2ToastComponent } from 'md2/toast';
+
+export const MD2_TOAST_DIRECTIVES: any[] = [Md2ToastComponent];
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, Md2ToastComponent],
     declarations: [AboutComponent],
-    exports: [AboutComponent]
+    exports: [AboutComponent, MD2_TOAST_DIRECTIVES]
 })
 
-export class AboutModule { }
+export class AboutModule {
+
+
+}
