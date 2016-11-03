6c471db19cc3354b4ee4afab63249da86a4d8568
amalshehu <amalshehu@gmail.com>
2016-11-03 12:57:06 +0530

    Add Md2 dependency

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -46,13 +46,12 @@ export class ProjectConfig extends SeedConfig {
      }
    });
 
-   // add Material configuration to SystemJS.
+   // add Md2 configuration to SystemJS.
   this.addPackageBundles({
     name:'md2',
     path:'node_modules/md2/md2.umd.js',
     packageMeta:{
       main: 'index.js',
-      format:'cjs',
       defaultExtension: 'js'
     }
   });
